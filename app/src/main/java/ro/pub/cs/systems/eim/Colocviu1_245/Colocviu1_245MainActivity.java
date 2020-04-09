28b0214fb46b5d276010d585b1a2eae469151a45
dianuta06 <diana.buzoianu@ejump.ro>
2020-04-09 14:35:14 +0300

    salvare stare functionala

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -2,12 +2,17 @@ package ro.pub.cs.systems.eim.Colocviu1_245;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Colocviu1_245MainActivity extends AppCompatActivity {
+
+    final public static int ADD_ELEMENTS = 12345;
 
     private ButtonClickListener buttonClickListener = new ButtonClickListener();
     private class ButtonClickListener implements View.OnClickListener {
@@ -34,5 +39,46 @@ public class Colocviu1_245MainActivity extends AppCompatActivity {
 
         Button buttonAdd = (Button) findViewById(R.id.button_add);
         buttonAdd.setOnClickListener(buttonClickListener);
+
+        Button buttonCompute = (Button) findViewById(R.id.button_compute);
+        buttonCompute.setOnClickListener(
+            new ButtonClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    TextView allTerms = (TextView)findViewById(R.id.editText_allTerms);
+
+                    Intent intent = new Intent("ro.pub.cs.systems.eim.Colocviu1_245.Colocviu1_245SecondaryActivity");
+                    intent.putExtra("toAdd", allTerms.getText().toString());
+
+                    startActivityForResult(intent, ADD_ELEMENTS);
+                }
+            }
+        );
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case ADD_ELEMENTS:
+                Bundle data = intent.getExtras();
+                Integer sum = data.getInt("sum");
+                Toast.makeText(this, "The activity returned with sum " + sum, Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        // apelarea metodei din activitatea parinte este recomandata, dar nu obligatorie
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        // apelarea metodei din activitatea parinte este recomandata, dar nu obligatorie
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -1,20 +1,42 @@
 package ro.pub.cs.systems.eim.Colocviu1_245;
 
-public class Colocviu1_245SecondaryActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
+
+    final public static int ADD_ELEMENTS = 12345;
 
     @Override
     protected void onCreate(Bundle state) {
         super.onCreate(state);
-        setContentView(R.layout.activity_another);
 
         // intent from parent
         Intent intentFromParent = getIntent();
         Bundle data = intentFromParent.getExtras();
         // process information from data ...
 
+        String toAdd = data.getString("toAdd");
+
+        toAdd =  toAdd.substring(0, toAdd.length() - 1);
+
+        int sum = 0;
+        for (int i = 0; i < toAdd.length(); i++) {
+            char a = toAdd.charAt(i);
+            if (Character.isDigit(a)) {
+                int b = Integer.parseInt(String.valueOf(a));
+                sum = sum + b;
+            }
+        }
+
         // intent to parent
         Intent intentToParent = new Intent();
-        intent.putExtra("ro.pub.cs.systems.eim.lab04.anotherKey", anotherValue);
+        intentToParent.putExtra("c", sum);
         setResult(RESULT_OK, intentToParent);
         finish();
     }
